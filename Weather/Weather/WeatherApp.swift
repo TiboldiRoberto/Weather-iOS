3e60a4b10ac537237f64c6df2c2ebb60a3f9423c
Roberto <roberto.tiboldi@ulbsibiu.ro>
2022-02-15 15:04:09 +0200

    Initial Project Setup

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Freak on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
