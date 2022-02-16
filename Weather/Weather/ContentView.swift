f43b74e9c08fbb8fcd3dec555ed5dd1428164eab
Roberto <roberto.tiboldi@ulbsibiu.ro>
2022-02-16 16:08:42 +0200

    Added city name at top

--- Weather/Weather/ContentView.swift
+++ Weather/Weather/ContentView.swift
@@ -5,17 +5,16 @@ struct ContentView: View {
     @StateObject var manager = LocationManager()
     
     var body: some View {
-        if let location = manager.location {
-            Text("Your location: \(location.latitude), \(location.longitude)")
-        }
-        
-        if let city = manager.city {
-            Text("Welcome to: \(city)")
-                .padding()
+        VStack{
+            if let city = manager.city {
+                Text("\(city)")
+                    .bold()
+                    .padding()
+            }
+            Spacer()
         }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
