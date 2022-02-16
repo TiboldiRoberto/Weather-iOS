78844c50a5a99407b0dd4b887d6e9755cb493203
Roberto <roberto.tiboldi@ulbsibiu.ro>
2022-02-16 15:30:44 +0200

    Permission for location and display current location

---
+++ Weather/Weather/LocationManager.swift
@@ -0,0 +1,47 @@
+import Foundation
+import CoreLocation
+import CoreLocationUI
+
+class LocationManager: NSObject,CLLocationManagerDelegate, ObservableObject {
+    private let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var city: String?
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        location = locations.first?.coordinate
+        
+        lookUpCurrentLocation { response in
+            self.city = response?.locality
+        }
+    }
+    
+    func lookUpCurrentLocation(completionHandler: @escaping (CLPlacemark?)
+                               -> Void ) {
+        if let lastLocation = self.manager.location {
+            let geocoder = CLGeocoder()
+    
+            geocoder.reverseGeocodeLocation(lastLocation,
+                                            completionHandler: { (placemarks, error) in
+                if error == nil {
+                    let firstLocation = placemarks?[0]
+                    completionHandler(firstLocation)
+                }
+                else {
+                    completionHandler(nil)
+                }
+            })
+        }
+        else {
+            completionHandler(nil)
+        }
+    }
+}
