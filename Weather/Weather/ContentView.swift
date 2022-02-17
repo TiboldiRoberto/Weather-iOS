c3f8c487ddd2e5dee343789467b09ffd70d570ea
Roberto <roberto.tiboldi@ulbsibiu.ro>
2022-02-17 10:38:32 +0200

    Completed app UI with icon, temperature, wind and clouds

--- Weather/Weather/ContentView.swift
+++ Weather/Weather/ContentView.swift
@@ -6,14 +6,63 @@ struct ContentView: View {
     
     var body: some View {
         VStack{
-            if let city = manager.city {
-                Text("\(city)")
+            
+            VStack{
+                if let city = manager.city {
+                    Text("\(city)")
+                        .font(.system(size: 20))
+                        .bold()
+                        .padding()
+                }
+            }
+            
+            Image("clouds")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150.0, height: 150.0)
+            
+            VStack{
+                Spacer()
+                Text("18°")
+                    .font(.system(size: 75))
+                    .padding()
+                HStack{
+                    Text("actual")
+                    Text("feelsLike")
+                    Text("min")
+                    Text("max")
+                }
+                Spacer()
+            }
+            
+            VStack{
+                Spacer()
+                Text("Wind")
                     .bold()
                     .padding()
+                HStack{
+                    Text("speed")
+                    Text("deg")
+                }
+                Spacer()
             }
+            
+            VStack{
+                Text("Clouds")
+                    .bold()
+                    .padding()
+                HStack{
+                    Text("all")
+                    Text("visibility")
+                    Text("humidity")
+                }
+                Spacer()
+            }
+            
             Spacer()
         }
     }
+    
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
